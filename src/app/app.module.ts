e2ab6660d044db77b35fa7dcc294e7cf8ef4f35e
Javier Quiñonero Gonzalez <javier.quinonero-gonzalez@capgemini.com>
2021-10-21 08:28:16 +0200

    Starting project + First configurations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './pages/shared/main/main.component';
+import { HeaderComponent } from './pages/shared/header/header.component';
+import { HeroComponent } from './pages/hero/hero.component';
+import { MissionComponent } from './pages/mission/mission.component';
+
+import {MatTabsModule} from '@angular/material/tabs';
+import { AppRoutingModule } from './app-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
+    HeroComponent,
+    MissionComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FlexLayoutModule,
+    MatTabsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
